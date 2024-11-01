d9e22e9a7a2a99424ba0194fccb02e52c13251c1
johan flinke <johanjagielka@gmail.com>
2024-11-01 22:43:07 +0100

    Added summary tab

--- src/components/scheduler/components/bottomMenu/BottomMenu.tsx
+++ src/components/scheduler/components/bottomMenu/BottomMenu.tsx
@@ -5,63 +5,101 @@ import { TextField } from "../textField/TextField";
 import styles from "./BottomMenu.module.scss";
 import { useColorLabels } from "../../../../hooks/useColorLabels";
 import { ColorDescription } from "./components/colorDescription/ColorDescription";
+import { ColorSummary } from "./components/colorSummary/ColorSummary";
 
 export const BottomMenu = () => {
   const { isEditMode, toggleEditMode } = useEditMode();
   const { cards, setCards } = useCards();
   const [textFieldset, setTextFieldset] = useState("");
   const { colorLabels } = useColorLabels();
+  const [active, setActive] = useState<"STATISTICS" | "EDIT">("STATISTICS");
 
   return (
     <div className={styles["bottom-menu"]}>
       <div>
-        <button
-          className={`${styles.toggle} ${
-            isEditMode && styles["toggle--active"]
-          }`}
-          type="button"
-          onClick={() => toggleEditMode()}
-        >
-          {isEditMode ? "Stop editing" : "Start editing"}
-        </button>
-        <button
-          className={styles.toggle}
-          type="button"
-          onClick={() => {
-            localStorage.removeItem("cards");
-            localStorage.removeItem("shifts");
-            localStorage.removeItem("row_labels");
-            localStorage.removeItem("col_labels");
-            localStorage.removeItem("color_labels");
-            window.location.reload();
-          }}
-        >
-          Reset
-        </button>
-        {isEditMode && (
-          <div>
-            <div className={styles["bottom-menu__button-wrapper"]}>
-              <TextField
-                setTextFieldset={setTextFieldset}
-                value={textFieldset}
-                label="Menu"
-              />
+        <div className={styles["tabs-wrapper"]}>
+          <ul className={styles["tabs"]}>
+            <li>
               <button
                 onClick={() => {
-                  const newCards = [...cards];
-                  textFieldset.split(",").forEach((word, i) => {
-                    if (typeof newCards[i] === "undefined") return;
-
-                    newCards[i].text = word;
-                  });
-                  setCards(newCards);
+                  setActive("STATISTICS");
                 }}
-                className={styles["bottom-menu__button"]}
+                className={`${styles["button"]} ${styles["tab"]} ${
+                  active === "STATISTICS" ? styles["tab--active"] : undefined
+                }`}
               >
-                Fill
+                Summary
               </button>
-            </div>
+            </li>
+            <li
+              className={`${styles["button"]} ${styles["tab"]} ${
+                active === "EDIT" ? styles["tab--active"] : undefined
+              }`}
+            >
+              <button
+                onClick={() => {
+                  setActive("EDIT");
+                }}
+                className={styles["button"]}
+              >
+                Edit
+              </button>
+            </li>
+          </ul>
+        </div>
+        {active === "EDIT" ? (
+          <div>
+            <button
+              className={`${styles.toggle} ${
+                isEditMode && styles["toggle--active"]
+              }`}
+              type="button"
+              onClick={() => toggleEditMode()}
+            >
+              {isEditMode ? "Stop editing" : "Start editing"}
+            </button>
+            <button
+              className={styles.toggle}
+              type="button"
+              onClick={() => {
+                localStorage.removeItem("cards");
+                localStorage.removeItem("shifts");
+                localStorage.removeItem("row_labels");
+                localStorage.removeItem("col_labels");
+                localStorage.removeItem("color_labels");
+                window.location.reload();
+              }}
+            >
+              Reset
+            </button>
+            {isEditMode && (
+              <div>
+                <div className={styles["bottom-menu__button-wrapper"]}>
+                  <TextField
+                    setTextFieldset={setTextFieldset}
+                    value={textFieldset}
+                    label="Menu"
+                  />
+                  <button
+                    onClick={() => {
+                      const newCards = [...cards];
+                      textFieldset.split(",").forEach((word, i) => {
+                        if (typeof newCards[i] === "undefined") return;
+
+                        newCards[i].text = word;
+                      });
+                      setCards(newCards);
+                    }}
+                    className={styles["bottom-menu__button"]}
+                  >
+                    Fill
+                  </button>
+                </div>
+              </div>
+            )}
           </div>
+        ) : (
+          <ColorSummary />
         )}
       </div>
       <div>
---
+++ src/components/scheduler/components/bottomMenu/components/colorSummary/ColorSummary.module.scss.d.ts
@@ -0,0 +1,8 @@
+declare const classNames: {
+  readonly "colors-summary": "colors-summary";
+  readonly "colors-list": "colors-list";
+  readonly color: "color";
+  readonly "color-box": "color-box";
+  readonly total: "total";
+};
+export = classNames;
