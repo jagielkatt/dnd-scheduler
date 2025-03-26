8aa221ac71347724dad4dc6b44408dba9d06d1fe
johan flinke <johanjagielka@gmail.com>
2025-03-26 21:46:53 +0100

    Added delete sheet button

--- src/components/scheduler/components/sheetBar/SheetBar.module.scss.d.ts
+++ src/components/scheduler/components/sheetBar/SheetBar.module.scss.d.ts
@@ -3,5 +3,6 @@ declare const classNames: {
   readonly "sheet-add-button": "sheet-add-button";
   readonly "sheet-button": "sheet-button";
   readonly "sheet-button--active": "sheet-button--active";
+  readonly "sheet-edit-button": "sheet-edit-button";
 };
 export = classNames;
